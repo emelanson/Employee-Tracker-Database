002b9d82238abf280c4d4259631d06953f91e64e
emelanson <elijahmelanson@gmail.com>
2021-02-16 01:19:48 -0500

    Updated schema.  Table columns and FKs should be correct. wrote seeds.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE database employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
@@ -13,8 +13,9 @@ CREATE TABLE role (
 id INT AUTO_INCREMENT,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL(12,2) NOT NULL,
+department_id INT NOT NULL,
 FOREIGN KEY (department_id)
-REFERENCES department(id)
+REFERENCES department(id),
 PRIMARY KEY (id)
 );
 
@@ -22,12 +23,9 @@ CREATE TABLE employee (
 id INT AUTO_INCREMENT,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
 FOREIGN KEY (role_id)
 REFERENCES role(id),
-FOREIGN KEY (manager_id)
-REFERENCES manager(id),
+manager_id INT REFERENCES employee(id),
 PRIMARY KEY (id)
 );
-
-
-INSERT management INTO 
