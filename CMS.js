e5e39d9a67bc85d20a3697d65535106a25c12a31
emelanson <elijahmelanson@gmail.com>
2021-02-17 14:12:10 -0500

    Completed view functions. Beginning Add functions. Updated scripts to allow for start via NPM start.

--- CMS.js
+++ CMS.js
@@ -3,99 +3,68 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "root",
     database: "employee_trackerdb"
 });
 
-
-
 connection.connect(err => {
-    console.log("=========CONNECTED TO EMPLOYEE MANAGEMENT SYSTEM======")
+    console.log("======CONNECTED TO EMPLOYEE MANAGEMENT SYSTEM======")
     if (err) throw err;
-
+    //Start program
     runManagement();
 });
 
-function runManagement() {
+/////////
+//INQUIRER MENUS
+/////////
+
+const runManagement = () => {
     inquirer
         .prompt({
             name: "action",
             type: "rawlist",
             message: "Welcome to the Employee Management System.  Select an action.",
             choices: [
-                "View Data",
+                "View Employees",
+                "View Roles",
+                "View Departments",
                 "Add Data",
                 "~~~EXIT~~~"
             ]
         })
         .then(answer => {
             switch (answer.action) {
-                case "View Data":
-                    viewData();
+                case "View Employees":
+                    viewEmployee();
+                    break;
+                case "View Roles":
+                    viewRole();
+                    break;
+                case "View Departments":
+                    viewDepartments();
                     break;
                 case "Add Data":
+                    addData();
                     break;
-
                 case "~~~EXIT~~~":
                     "EXITING EMPLOYEE CMS"
+                    //end connection and exit program
                     connection.end;
                     process.exit();
             };
         });
 };
 
-function viewData() {
-    inquirer
-        .prompt({
-            name: "data",
-            type: "rawlist",
-            message: "What category of data would you like to view?",
-            choices: [
-                "role",
-                "employee",
-                "department",
-                "Go Back"
-            ]
-        })
-        .then(answer => {
-            if (answer.data !== "Go Back") {
-                let query = answer.data;
 
-                switch (answer.data) {
-                    case "role":
-                        viewRole();
-                        break;
-                    case "employee":
-                        viewEmployee();
-                        break;
-                    case "department":
-                        connection.query("SELECT * FROM department", (err, res) => {
-                            if (err) console.log(err);
-                            console.log(`\n`)
-                            console.table(res)
-                            console.log(`\n`)
-                        });
 
-                        break;
-
-                    default:
-                        break;
-                }
-
-            };
-        });
-};
+////////////
+//VIEW FUNCTIONS
+////////////
 
 const viewRole = () => {
-    connection.query(`SELECT * 
+    connection.query(`SELECT role.title, role.salary, department.name AS "Department Name"
     FROM role
     INNER JOIN department ON (role.department_id = department.id)`, (err, res) => {
         if (err) console.log(err);
@@ -106,10 +75,21 @@ const viewRole = () => {
     });
 };
 
+const viewDepartments = () => {
+    connection.query("SELECT * FROM department", (err, res) => {
+        if (err) console.log(err);
+        console.log(`\n`)
+        console.table(res)
+        console.log(`\n`)
+        runManagement();
+    });
+};
+
 const viewEmployee = () => {
-    connection.query(`SELECT employee.id, employee.first_name AS 'First Name', employee.last_name AS 'Last Name', role.title, role.salary, CONCAT(Manager.first_name, ' ', Manager.last_name) AS 'Manager', employee.manager_id 
+    connection.query(`SELECT employee.id, employee.first_name AS 'First Name', employee.last_name AS 'Last Name', role.title, department.name AS "Department Name", role.salary, CONCAT(Manager.first_name, ' ', Manager.last_name) AS 'Manager', employee.manager_id 
     FROM employee 
     INNER JOIN role ON (employee.role_id = role.id)
+    INNER JOIN department ON (role.department_id = department.id)
     LEFT JOIN employee AS Manager On (employee.manager_id = Manager.id);`, (err, res) => {
         if (err) console.log(err);
         console.log("Employee View")
@@ -118,3 +98,8 @@ const viewEmployee = () => {
         runManagement();
     });
 };
+
+/////////
+//ADD FUNCTIONS
+/////////
+
